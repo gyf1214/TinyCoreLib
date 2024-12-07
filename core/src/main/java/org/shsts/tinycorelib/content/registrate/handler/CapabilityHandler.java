cb5efc589a48d2771ac50c3ce4058a8eb057cdf1
gyf1214 <gyf1214@gmail.com>
2024-12-06 16:30:00 -0800

    Improve logging in CapabilityHandler

--- core/src/main/java/org/shsts/tinycorelib/content/registrate/handler/CapabilityHandler.java
+++ core/src/main/java/org/shsts/tinycorelib/content/registrate/handler/CapabilityHandler.java
@@ -1,11 +1,13 @@
 package org.shsts.tinycorelib.content.registrate.handler;
 
+import com.mojang.logging.LogUtils;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import org.shsts.tinycorelib.content.registrate.Registrate;
 import org.shsts.tinycorelib.content.registrate.entry.CapabilityEntry;
+import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +15,20 @@ import java.util.List;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class CapabilityHandler {
-    private final Registrate registrate;
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private final String modid;
     private final List<Class<?>> capabilities = new ArrayList<>();
 
     public CapabilityHandler(Registrate registrate) {
-        this.registrate = registrate;
+        this.modid = registrate.modid;
     }
 
     public void onRegisterEvent(RegisterCapabilitiesEvent event) {
+        if (capabilities.isEmpty()) {
+            return;
+        }
+        LOGGER.info("Mod {} register {} capabilities", modid, capabilities.size());
         for (var cap : capabilities) {
             event.register(cap);
         }
@@ -29,6 +37,6 @@ public class CapabilityHandler {
 
     public <T> CapabilityEntry<T> register(Class<T> clazz, CapabilityToken<T> token) {
         capabilities.add(clazz);
-        return new CapabilityEntry<>(registrate.modid, token);
+        return new CapabilityEntry<>(modid, token);
     }
 }
