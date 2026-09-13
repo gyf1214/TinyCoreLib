7d739a8528702ffff4f6850774674617330b0e78
gyf1214 <gyf1214@gmail.com>
2026-09-12 18:38:57 -0700

    Add RegistryEntry.key

--- core/src/main/java/org/shsts/tinycorelib/api/registrate/entry/IRegistry.java
+++ core/src/main/java/org/shsts/tinycorelib/api/registrate/entry/IRegistry.java
@@ -3,10 +3,13 @@ package org.shsts.tinycorelib.api.registrate.entry;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import org.shsts.tinycorelib.api.registrate.handler.IEntryHandler;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public interface IRegistry<V> extends IEntry<Registry<V>> {
+    ResourceKey<Registry<V>> key();
+
     IEntryHandler<V> getHandler();
 }
--- core/src/main/java/org/shsts/tinycorelib/content/registrate/entry/RegistryEntry.java
+++ core/src/main/java/org/shsts/tinycorelib/content/registrate/entry/RegistryEntry.java
@@ -4,6 +4,7 @@ import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import org.shsts.tinycorelib.api.registrate.entry.IRegistry;
 import org.shsts.tinycorelib.api.registrate.handler.IEntryHandler;
@@ -11,17 +12,23 @@ import org.shsts.tinycorelib.content.registrate.handler.EntryHandler;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class RegistryEntry<V> extends Entry<Registry<V>>
-    implements IRegistry<V> {
+public class RegistryEntry<V> extends Entry<Registry<V>> implements IRegistry<V> {
+    private final ResourceKey<Registry<V>> key;
     @Nullable
     private EntryHandler<V> handler;
 
     public RegistryEntry(ResourceLocation loc) {
         super(loc);
+        this.key = ResourceKey.createRegistryKey(loc);
     }
 
     public void setHandler(EntryHandler<V> value) {
         handler = value;
+    }
+
+    @Override
+    public ResourceKey<Registry<V>> key() {
+        return key;
     }
 
     @Override
